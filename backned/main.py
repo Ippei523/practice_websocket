13f82cb2cc37cadf1da0bbb4c75722fb7c1012eb
Ippei523 <ippei523@gmail.com>
2024-10-12 18:15:54 +0900

    load_env()で読み込むことができる

--- backned/main.py
+++ backned/main.py
@@ -2,6 +2,9 @@ from flask import Flask
 from backned.database.database import db
 from flask_socketio import SocketIO
 from views import message_view
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret!"
