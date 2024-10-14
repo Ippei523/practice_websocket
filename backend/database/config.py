ac488639065f1eceaab537340a19611dad4ee6b7
Ippei523 <ippei523@gmail.com>
2024-10-14 16:34:29 +0900

    postgre用に変更

--- backend/database/config.py
+++ backend/database/config.py
@@ -6,9 +6,10 @@ USER_NAME = os.environ.get("USER_NAME")
 PASSWORD = os.environ.get("PASSWORD")
 HOST = os.environ.get("HOST")
 DATABASE_NAME = os.environ.get("DATABASE_NAME")
-DATABASE_URL = f"mysql+pymysql://{USER_NAME}:{PASSWORD}@{HOST}/{DATABASE_NAME}"
+DATABASE_URL = f"postgresql+psycopg2://{USER_NAME}:{PASSWORD}@{HOST}/{DATABASE_NAME}"
 
 
 class Config:
+    print(f"DATABASE_URL: {DATABASE_URL}")
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
