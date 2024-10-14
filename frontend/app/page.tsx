2fe7d313f7b61379a39a43d34dcfd80ee03401ba
Ippei523 <ippei523@gmail.com>
2024-10-14 17:37:30 +0900

    フロント側の編集

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,19 @@
-import styles from "./page.module.css";
+import PageTitle from "@/components/PageTitle";
+import { Box, ChakraProvider, Link } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      
-    </main>
+    <ChakraProvider>
+      <Box>
+        <PageTitle title="Home" />
+      </Box>
+      {/* いい感じにデザインして欲しい */}
+      <Box display={"flex"} flexDir={"column"}>
+        {/* ページ遷移させたい */}
+        <Link href="/user-info">ユーザー情報</Link>
+        <Link href="/friend">友達</Link>
+        <Link href="/chat">チャット</Link>
+      </Box>
+    </ChakraProvider>
   );
 }
