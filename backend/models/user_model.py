21fc31a635078e5c095e3c20d35780224b922f94
Ippei523 <ippei523@gmail.com>
2024-10-14 16:58:55 +0900

    ユーザーモデルの作成

---
+++ backend/models/user_model.py
@@ -0,0 +1,26 @@
+from database.database import db
+from flask_sqlalchemy import SQLAlchemy
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(
+        db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now()
+    )
+
+    def __init__(self, username, password):
+        super().__init__()
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f"<User {self.id}>"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+        }
