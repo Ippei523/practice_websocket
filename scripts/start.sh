30d5fd4b353bd44312483d760144e0f99c53db30
Ippei523 <ippei523@gmail.com>
2024-10-14 16:32:08 +0900

    仮装環境のセットアップを毎回行う

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
+# 古い仮想環境を削除
+rm -rf backend/venv
+echo "Old virtual environment removed"
+
+# 仮装環境を作成
 python3 -m venv backend/venv
-pip install -r backend/requirements.txt
-source backend/venv/bin/activate
-python3 backend/main.py
+echo "Virtual environment created"
+
+# ディレクトリに移動
+cd backend
+
+# 仮想環境をアクティブにする
+echo "Creating virtual environment ..."
+source venv/bin/activate
+
+# 必要なライブラリをインストール
+echo "Installing requirements ..."
+pip install -r requirements.txt
+
+# アプリケーションを起動
+echo "Starting the application ..."
+python3 main.py
+echo "Application started"
